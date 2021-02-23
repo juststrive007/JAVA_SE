4925014b26d90b78d2f6003788f0ae42555ed07a
juststrive007 <juststrive007@gmail.com>
2021-02-23 09:11:18 +0800

    推送

--- src/map/MapDemo1.java
+++ src/map/MapDemo1.java
@@ -1,7 +1,17 @@
 package map;
 
 /**
- * 
+ * java.util.Map接口 查找表
+ * Map体现的结构是一个多行两列的表格，左列称为key，右列称为value
+ * Map总是根据key来获取对应得value。因此保存数据时是成对保存的，并且
+ * 应当将查询的数据作为value查询条件作为key进行保存。
+ * Map要求key不允许重复（equals比较判定）
+ *
+ * 常用实现类：
+ * java.util.HashMap,散列表 哈希表
+ * 当今查询速度最快的数据结构。
+ * @author wm
  */
 public class MapDemo1 {
+
 }
